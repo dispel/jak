d5da8f3a09ab3294d0b587ac5a83a2a277e18c78
Chris DiLorenzo <cdilorenzo@dispel.io>
2017-01-25 15:26:22 -0500

    jak can now read any file (I think). I tested it with a files up to 50MB. For very huge files it will fail because it loads them into memory wholesale for encryption and decryption.

--- jak/aes_cipher.py
+++ jak/aes_cipher.py
@@ -107,7 +107,7 @@ class AES256Cipher(object):
         raise Exception('FATAL: No one should end up here.... VERSION: {}, C: {}'.format(version, ciphertext))
 
     def decrypt(self, ciphertext):
-        """Decrypts an ciphertext secret."""
+        """Decrypts a ciphertext secret"""
 
         # This allows us to upgrade the encryption and MAC
         version = self._extract_version(ciphertext=ciphertext)
@@ -129,10 +129,6 @@ class AES256Cipher(object):
 
     def encrypt(self, plaintext, iv=False):
         """Encrypts a plaintext secret"""
-
-        # Convert secret to bytes (FIXME: why not just read it as bytes?)
-        plaintext = plaintext.encode()
-
         if not iv:
             iv = self._generate_iv()
 
