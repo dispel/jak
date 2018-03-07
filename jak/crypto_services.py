fdcc294ffab7280089d6d013808c204cce5d812e
Chris DiLorenzo <cdilorenzo@dispel.io>
2018-03-07 16:50:40 -0500

    updating copyright to be 2018.

--- jak/crypto_services.py
+++ jak/crypto_services.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright 2017 Dispel, LLC
+Copyright 2018 Dispel, LLC
 Apache 2.0 License, see https://github.com/dispel/jak/blob/master/LICENSE for details.
 """
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 Documentation: https://jak.readthedocs.io
 
-Copyright 2017 Dispel, LLC
+Copyright 2018 Dispel, LLC
 
 Apache 2.0 License, see https://github.com/dispel/jak/blob/master/LICENSE for details.
 """
